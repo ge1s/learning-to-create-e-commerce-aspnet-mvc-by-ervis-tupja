85705fd7dcab394939cddfa54675bd3d15a8d9d4
ge1s <geisanebiu1@gmail.com>
2024-02-23 00:18:08 +0100

    Trying to commit the first model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Biography { get; set; }
+    }
+}
